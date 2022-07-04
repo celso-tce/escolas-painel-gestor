d4f7c13b96eb2519ed575fac20bf03e993e560c1
Celso Emmanuel <celso.filgueiras@tce.ap.gov.br>
2022-07-04 13:19:10 -0300

    filtro de role no user table

--- components/users/UserTable.tsx
+++ components/users/UserTable.tsx
@@ -1,5 +1,7 @@
 import { User } from "escolas-shared";
 import React from 'react';
+import ReactSelect, { SingleValue } from "react-select";
+import { SelectUtils, UserRoleOptionType } from "../../lib/ui-utils";
 import { Utils } from "../../lib/utils";
 import ResourceTable, { BasicResourceTableProps } from "../resources/ResourceTable";
 import FormItem from "../ui/forms/FormItem";
@@ -8,8 +10,16 @@ import Label from "../ui/inputs/Label";
 
 type UserTableProps = BasicResourceTableProps<User>;
 
+const userRoleOptions = [
+  { value: undefined, label: 'Todos' },
+  ...SelectUtils.userRoleOptions,
+];
+
 const UserTable: React.FC<UserTableProps> = (props) => {
   const [filterNome, setFilterNome] = React.useState('');
+
+  const [filterRoleOption, setFilterRoleOption] =
+    React.useState<SingleValue<UserRoleOptionType>>(userRoleOptions[0]);
 
   return (
     <ResourceTable
@@ -26,23 +36,30 @@ const UserTable: React.FC<UserTableProps> = (props) => {
       ]}
       applyFilter={(resources) => {
         const filterNomeLower = filterNome.trim().toLowerCase();
-        const hasFilterNomeLower = filterNomeLower ? true : false;
+        const filterRole = filterRoleOption?.value;
 
-        if (!hasFilterNomeLower)
+        const hasFilterNomeLower = filterNomeLower ? true : false;
+        const hasFilterRole = filterRole !== undefined;
+
+        if (!hasFilterNomeLower && !hasFilterRole)
           return resources;
 
-        return resources.filter((categoria) => {
+        return resources.filter((user) => {
           let pass = true;
 
           if (hasFilterNomeLower)
-            pass &&= categoria.nome.toLocaleLowerCase().includes(filterNomeLower)
-              || categoria.email.toLocaleLowerCase().includes(filterNomeLower)
-              || categoria.role.toLocaleLowerCase().includes(filterNomeLower);
+            pass &&= user.nome.toLocaleLowerCase().includes(filterNomeLower)
+              || user.email.toLocaleLowerCase().includes(filterNomeLower)
+              || user.role.toLocaleLowerCase().includes(filterNomeLower);
+
+          if (hasFilterRole) {
+            pass &&= user.role === filterRole;
+          }
 
           return pass;
         });
       }}
-      filtersContent={(
+      filtersContent={(<>
         <FormItem className="lg:w-4/12 px-4">
           <Label label="Pesquisar" htmlFor="filter-busca" />
           <Input
@@ -53,7 +70,17 @@ const UserTable: React.FC<UserTableProps> = (props) => {
             debounceTimeMs={250}
           />
         </FormItem>
-      )}
+
+        <FormItem className="lg:w-3/12 px-4">
+          <Label label="Papel" htmlFor="filter-role" />
+          <ReactSelect
+            id="filter-role"
+            options={userRoleOptions}
+            value={filterRoleOption}
+            onChange={(newValue) => setFilterRoleOption(newValue)}
+          />
+        </FormItem>
+      </>)}
     />
   );
 };
